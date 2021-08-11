18fe771b5de687b74c9f1f88ddc29197b63aa29a
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2021-08-10 22:43:31 -0300

    import node

---
+++ main.js
@@ -0,0 +1,4 @@
+// TODO: Use express to show charts, etc.
+// also, start making the images, etc. api for the bot.
+
+// for the image api, the bot could make a request for the api, providing name, etc. and then the api outputs the image requested
